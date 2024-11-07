6dc70f54399e7016068bfa8aec3d54ffb2c47bb0
Anderson  Henrique Lucena Do Nascismento Heraclio <alucena933@gmail.com>
2024-11-07 10:00:11 -0300

    refactor: :sparkles: Criação de pastas e arquivos para proximas features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import React from "react";
+import "./Styles/global.css";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <div className="App">Wellouu word</div>;
 }
 
 export default App;
